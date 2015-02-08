c456770b0d9cd430ff781af0b9a5bd97f76fd38c
Keisuke Nishida <keisuke.nishida@gmail.com>
2015-02-08 20:55:48 +0900

    Add tests for targets.tableau

--- luigi_td/targets/tableau.py
+++ luigi_td/targets/tableau.py
@@ -9,36 +9,35 @@ __all__ = ['TableauServerResultTarget', 'TableauOnlineResultTarget']
 
 class TableauServerResultTarget(ResultTarget):
     # required
-    tableau_server = None
-    tableau_username = None
-    tableau_password = None
-    tableau_datasource = None
+    server = None
+    username = None
+    password = None
+    datasource = None
     # optional
-    tableau_ssl = 'true'
-    tableau_ssl_verify = 'true'
-    tableau_version = None
-    tableau_site = None
-    tableau_project = None
-    tableau_mode = 'replace'
+    ssl = 'true'
+    ssl_verify = 'true'
+    version = None
+    site = None
+    project = None
+    mode = 'replace'
 
     def get_result_url(self):
         reqs = {}
-        for key in ['server', 'username', 'password', 'datasource']:
-            name = 'tableau_' + key
+        for name in ['server', 'username', 'password', 'datasource']:
             if getattr(self, name) is None:
                 raise TypeError('missing option "{0}" for {1}'.format(name, self))
-            reqs[key] = urllib.quote(getattr(self, name))
+            reqs[name] = urllib.quote(getattr(self, name))
         params = {
-            'ssl': self.tableau_ssl,
-            'ssl_verify': self.tableau_ssl_verify,
-            'version': self.tableau_version,
-            'site': self.tableau_site,
-            'project': self.tableau_project,
-            'mode': self.tableau_mode,
+            'ssl': self.ssl,
+            'ssl_verify': self.ssl_verify,
+            'version': self.version,
+            'site': self.site,
+            'project': self.project,
+            'mode': self.mode,
         }
         reqs['params'] = urllib.urlencode([(key, params[key]) for key in params if params[key] is not None])
         return "tableau://{username}:{password}@{server}/{datasource}?{params}".format(**reqs)
 
 class TableauOnlineResultTarget(TableauServerResultTarget):
-    tableau_server = 'online.tableausoftware.com'
-    tableau_version = 'online'
+    server = 'online.tableausoftware.com'
+    version = 'online'
---
+++ luigi_td/targets/test_tableau.py
@@ -0,0 +1,76 @@
+from ..test_helper import TestEnv, MockJob
+from tableau import TableauServerResultTarget
+from tableau import TableauOnlineResultTarget
+
+from unittest import TestCase
+from nose.tools import eq_, raises
+
+import urllib
+import urlparse
+
+env = TestEnv()
+
+class TestTableauServerResultTarget(TableauServerResultTarget):
+    server = 'tableau.example.com'
+    username = 'test@example.com'
+    password = 'test-password'
+
+class TableauServerResultTargetTestCase(TestCase):
+    def setUp(self):
+        env.setUp()
+
+    def tearDown(self):
+        env.tearDown()
+
+    def test_default(self):
+        target = TestTableauServerResultTarget(env.get_tmp_path('result.job'))
+        target.datasource = 'test-datasource'
+        url = urlparse.urlparse(target.get_result_url())
+        params = urlparse.parse_qs(url.query)
+        eq_(url.scheme, 'tableau')
+        eq_(url.hostname, 'tableau.example.com')
+        eq_(url.path, '/' + target.datasource)
+        eq_(urllib.unquote(url.username), TestTableauServerResultTarget.username)
+        eq_(urllib.unquote(url.password), TestTableauServerResultTarget.password)
+        eq_(params.get('ssl'), ['true'])
+        eq_(params.get('ssl_verify'), ['true'])
+        eq_(params.get('version'), None)
+        eq_(params.get('site'), None)
+        eq_(params.get('project'), None)
+        eq_(params.get('mode'), ['replace'])
+
+    def test_options(self):
+        target = TestTableauServerResultTarget(env.get_tmp_path('result.job'))
+        target.version = '8.3'
+        target.site = 'test-site'
+        target.project = 'test-project'
+        target.datasource = 'test-datasource'
+        target.mode = 'append'
+        url = urlparse.urlparse(target.get_result_url())
+        params = urlparse.parse_qs(url.query)
+        eq_(params.get('version'), [target.version])
+        eq_(params.get('site'), [target.site])
+        eq_(params.get('project'), [target.project])
+        eq_(params.get('mode'), [target.mode])
+
+class TableauOnlineResultTargetTestCase(TestCase):
+    def setUp(self):
+        env.setUp()
+
+    def tearDown(self):
+        env.tearDown()
+
+    def test_default(self):
+        target = TableauOnlineResultTarget(env.get_tmp_path('result.job'))
+        target.username = 'test@example.com'
+        target.password = 'test-password'
+        target.datasource = 'test-datasource'
+        url = urlparse.urlparse(target.get_result_url())
+        params = urlparse.parse_qs(url.query)
+        eq_(url.scheme, 'tableau')
+        eq_(url.hostname, 'online.tableausoftware.com')
+        eq_(url.path, '/' + target.datasource)
+        eq_(urllib.unquote(url.username), target.username)
+        eq_(urllib.unquote(url.password), target.password)
+        eq_(params['version'], ['online'])
+        eq_(params['mode'], ['replace'])
