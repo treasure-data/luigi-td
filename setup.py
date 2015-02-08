9a7480bb11ba369e3ee70dbbcd2ebf6b7cb92219
Keisuke Nishida <keisuke.nishida@gmail.com>
2015-02-08 16:32:44 +0900

    v0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="luigi-td",
-    version='0.0.0.dev7',
+    version='0.1.0',
     description="Luigi integration for Treasure Data",
     author="Treasure Data, Inc.",
     author_email="support@treasure-data.com",
