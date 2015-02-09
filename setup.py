ee8b5b5cc1a47213180e7c4ccf8b416202d66fa9
Keisuke Nishida <knishida@treasure-data.com>
2015-02-09 21:53:12 +0900

    v0.6.1

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="luigi-td",
-    version='0.6.0',
+    version='0.6.1',
     description="Luigi integration for Treasure Data",
     author="Treasure Data, Inc.",
     author_email="support@treasure-data.com",
-    url="http://treasuredata.com/",
+    url="https://github.com/k24d/luigi-td",
     install_requires=open("requirements.txt").read().splitlines(),
     packages=find_packages(),
-    license="Apache Software License",
+    license="Apache License 2.0",
     platforms="Posix; MacOS X; Windows",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Topic :: Internet",
+        "Topic :: Software Development",
     ],
 )
