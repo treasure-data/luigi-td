3dfc534025ad1c301f03197b8f956cb19db3d4aa
Yamashita, Yuu <peek824545201@gmail.com>
2017-09-26 05:03:05 +0000

    v0.6.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="luigi-td",
-    version='0.6.7',
+    version='0.6.8',
     description="Luigi integration for Treasure Data",
     author="Treasure Data, Inc.",
     author_email="support@treasure-data.com",
